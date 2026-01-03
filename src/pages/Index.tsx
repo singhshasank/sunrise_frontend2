d22868d0a304b0c0e868a1eeabd22faa208fb6be
singhshasank <166588692+singhshasank@users.noreply.github.com>
2026-01-03 11:19:48 +0530

    Update Index.tsx
    ADDED IMAGE OF DIRECTOR USING URL

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -80,7 +80,7 @@ const Index = () => {
       <DirectorMessage />
       
       <section className="py-16 md:py-24 bg-white overflow-hidden relative">
-        <div className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')] bg-cover bg-center opacity-5"></div>
+        <div className="absolute inset-0 bg-[url('[url=https://ibb.co/B5662sRK][img]https://i.ibb.co/B5662sRK/Whats-App-Image-2026-01-03-at-11-15-12-AM.jpg[/img][/url]')] bg-cover bg-center opacity-5"></div>
         
         <div className="container mx-auto px-4 relative">
           <div className="max-w-4xl mx-auto text-center">
